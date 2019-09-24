86e332973a8b821ecda7a8744ba9a9e1bc2126a1
wxg0913 <2454622984@qq.com>
2019-09-24 16:21:01 +0800

    相当于命令行的 gitadd加上commmit操作

--- 01.helloWorld.js
+++ 01.helloWorld.js
@@ -1,3 +1,4 @@
 var mysql = require("mysql"); // 引入 mysql 模块     require： commonJs 模块引入方式
 console.log(mysql); // 直接在终端输入 node 01.helloWorld.js 可以查看输出内容
-console.log("vscode合作代码提交测试");
+console.log("vscode合作代码提交测试"); // 这是最新的文件 刚才右下角问题提示了 // 我已经Gitpull 代码是最新的
+console.log(111111) //教你别 的方式提交
