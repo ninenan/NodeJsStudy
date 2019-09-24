7b0c7ad5a56eaa05cb6ee1503ad07b90268e5f7d
NineNan <2441277210@qq.com>
2019-09-24 15:23:28 +0800

    vscode合作代码提交测试

--- 01.helloWorld.js
+++ 01.helloWorld.js
@@ -1,2 +1,3 @@
-var mysql = require('mysql'); // 引入 mysql 模块     require： commonJs 模块引入方式
+var mysql = require("mysql"); // 引入 mysql 模块     require： commonJs 模块引入方式
 console.log(mysql); // 直接在终端输入 node 01.helloWorld.js 可以查看输出内容
+console.log("vscode合作代码提交测试");
